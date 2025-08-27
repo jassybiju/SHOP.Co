b012384dfd5b1b4019c136b0811f3271279fb6c0
jassybiju <jassymon114@gmail.com>
2025-08-27 08:47:52 +0530

    feat (auth) : Add User model

---
+++ backend/models/user.model.js
@@ -0,0 +1,66 @@
+import mongoose, { Schema } from "mongoose";
+import * as bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      default: null,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    dob: {
+      type: Date,
+      required: true,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    last_login: {
+      type: DateTime,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+try {
+    let user = this;
+  
+    //checking if password is modified
+    if (!user.isModified("password")) return next();
+  
+    //generating salt
+    const SALT_WORK_FACTOR = process.env.SALT_WORK_FACTOR || 10;
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+  
+    //hashing password
+    const hash = bcrypt.hash(user.password, salt);
+    user.password = hash;
+    next();
+} catch (error) {
+  next(error)
+}
+});
+
+userSchema.methods.comparePassword =async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+const User = mongoose.model('user', userSchema)
